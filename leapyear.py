7df7f8f6643ec37733e9e07ca05dcebca74b608d
hotchkma <63124213+hotchkma@users.noreply.github.com>
2021-05-22 19:31:21 -0700

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,11 @@
+def check(n):
+     if n%4==0:
+         if n%100==0:
+                 if n%400==0:
+                         print(f"{n} is a leap year")
+                 else:
+                         print(f"{n} is not a leap year")
+         else:
+             print(f"{n} is a leap year")
+     else:
+         print(f"{n} is not a leap year")
